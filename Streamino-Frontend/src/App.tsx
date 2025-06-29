a49b4bed74e2a9bb8bd6dc370dab772bab33b466
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2025-06-30 00:04:39 +0530

    add Tailwind config and basic form UI without styling

--- Streamino-Frontend/src/App.tsx
+++ Streamino-Frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Stream from './pages/Stream';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import VideoPlayer from './components/VideoPlayer';
+//import VideoPlayer from './components/VideoPlayer';
 
 
 
@@ -13,8 +13,7 @@ import VideoPlayer from './components/VideoPlayer';
 const App:React.FC = () => {
   return (
     <Router>
-      <Navbar/>
-      <VideoPlayer/>
+      
       
       <Routes>
         <Route path='/' element={<Home/>}/>
--- Streamino-Frontend/src/pages/Register.tsx
+++ Streamino-Frontend/src/pages/Register.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Register:React.FC = () => {
 	return(
 		<div>
-			Hello Register 
+			<form action="">
+				<input type="text" placeholder='Full Name' />
+				
+				<input type='text' placeholder='Username'/>
+				
+				<input type='text' placeholder='Email'/>
+				
+				
+				<input type='text' placeholder='Password'/>
+				
+				<input type='text' placeholder='Confirm Password'/>
+				
+				<button type='submit'>
+					Register
+				</button>
+				 
+			</form>
+			
+			
+			<p>Already have an account?
+				<Link to="/login">
+					Login
+				</Link>
+			</p>
 		</div>
 	)
 }
