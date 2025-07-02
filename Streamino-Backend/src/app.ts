66259a838642c85467758fd6a70062c49418f482
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2025-07-02 23:46:32 +0530

    fix: remove submodule and add backend as normal folder

---
+++ Streamino-Backend/src/app.ts
@@ -0,0 +1,21 @@
+import express  from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req , res) => {
+	res.send("Streamino backend api is runnning");
+});
+
+
+export default app;
+
+
+
---
+++ Streamino-Backend/src/server.ts
@@ -0,0 +1,27 @@
+// src/server.ts
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config(); // ✅ Load environment variables
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error('❌ MONGO_URI is not defined in .env');
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  });
